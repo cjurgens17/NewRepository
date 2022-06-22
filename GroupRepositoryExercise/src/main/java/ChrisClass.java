32839136709c0d1b1c135817597d70a16bf20590
nguyr <duymisc@gmail.com>
2022-06-22 12:18:12 -0500

    Adding file

--- GroupRepositoryExercise/src/main/java/ChrisClass.java
+++ GroupRepositoryExercise/src/main/java/ChrisClass.java
@@ -1,2 +1,23 @@
 public class ChrisClass {
+    public static void main(String[] args) {
+        int x = 2;
+        switch(x*4){
+            case 1:
+                System.out.println("This is case 1");
+                break;
+
+            case 2:
+                System.out.println("This is case 2");
+                break;
+
+            default:
+                System.out.println("This is default");
+        }
+
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+
+
 }
