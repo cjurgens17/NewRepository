783f902b059eda4820b602bfa8ffcf3584a6703f
mapearcy <mapearcy@gmail.com>
2022-06-22 12:12:30 -0500

    adding Mark Branch

---
+++ GroupRepositoryExercise/src/main/java/MarkClass.java
@@ -0,0 +1,12 @@
+package GroupRepositoryExercise.src.main.java;
+
+public class MarkClass {
+    public static void main(String[] args) {
+        //access class methods here
+
+
+        System.out.println("Mark");
+
+    }
+
+}
