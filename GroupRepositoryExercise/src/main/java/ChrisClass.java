3594612633448a60a4ebcc3c7dbbafc6f6c8d286
chris <104324755+cjurgens17@users.noreply.github.com>
2022-06-22 13:12:19 -0400

    update

--- GroupRepositoryExercise/src/main/java/ChrisClass.java
+++ GroupRepositoryExercise/src/main/java/ChrisClass.java
@@ -1,2 +1,48 @@
+import java.util.SortedMap;
+
 public class ChrisClass {
+    //fields
+    private int age;
+    private String name;
+
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String s) {
+        s = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int a) {
+        a = age;
+    }
+    //methods
+
+
+    public void charCity() {
+        char a = '0';
+
+        for (int i = 0; i < 79; i++) {
+            System.out.print(a);
+            a++;
+        }
+        System.out.println();
+    }
+
+    public void howOld(int age) {
+        if (age % 2 == 1) {
+            System.out.println("You have an odd age!");
+        } else if (age % 2 == 0) {
+            System.out.println("You have an even age!");
+        } else {
+            System.out.println("Your an alien");
+        }
+
+
+    }
 }
---
+++ GroupRepositoryExercise/src/main/java/MainChris.java
@@ -0,0 +1,7 @@
+public class MainChris {
+    public static void main(String[] args) {
+        ChrisClass c = new ChrisClass();
+        c.charCity();
+        c.howOld(26);
+    }
+}
