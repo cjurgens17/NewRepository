c2764fb87349b5072fcae2e75dd4538d41d3160b
jordan powell <jordan.powell1025@gmail.com>
2022-06-22 12:18:53 -0500

    This has moved

---
+++ GroupRepositoryExercise/JordanClass.java
@@ -0,0 +1,14 @@
+public class JordanClass {
+
+    public static void main(String[]args){
+
+
+        //for loop that prints numbers 1-10
+
+        for(int i =1;i<10;i++){
+
+            System.out.println(i);
+
+        }
+    }
+}
